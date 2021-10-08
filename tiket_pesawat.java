aa17d276fa7e388f2c53424f4b12d2cf4448c2cc
andiishak67 <91652502+andiishak67@users.noreply.github.com>
2021-10-09 02:00:25 +0800

    Add files via upload

---
+++ tiket_pesawat.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class tiket_pesawat{
+
+public static void main (String[]args){
+
+Scanner input = new Scanner(System.in);
+
+    String nama , alamat;
+    int tipe_pesawat , bayar;
+    
+    System.out.print ("masukkan nama anda :");	
+		 nama = input.nextLine(); 
+	System.out.print ("masukkan alamat anda :");	
+		 alamat = input.nextLine(); 
+
+ boolean loop = true;
+System.out.println("menu pilihan:");
+System.out.println("1.nasi goreng ayam");
+System.out.println("2.nasi goreng seafood");
+System.out.println("3.soto ayam");
+System.out.println("4.jus jeruk");
+System.out.println("pilihan:");
+pilihan = input.nextInt();
+		 
+		 }
+		 }
